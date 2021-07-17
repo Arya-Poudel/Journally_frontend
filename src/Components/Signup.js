af6397e0f489bc7933d2c7e4fc877a4a4e09a340
Arya Poudel <arya.poudel20@gmail.com>
2021-07-17 16:26:25 +0545

    Login after signup

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
 import HomeNav from './HomeNav';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
 const Signup = () => {
 
+	const history = useHistory();
 	const [message, setMessage] = useState('');
 
 	const handleFormSubmit = async (e) => {
@@ -24,8 +25,33 @@ const Signup = () => {
 				//serialize the JSON body
 				body: JSON.stringify(Object.fromEntries(formdata))
 			})
-			const data = await response.json();
-			setMessage(data.message);
+			if (response.ok) {
+				try{
+					const response = await fetch('https://journally-backend.herokuapp.com/login', {
+						method: "POST",
+						mode: 'cors',
+						headers:{
+							'Accept': 'application/json',
+							'Content-type': 'application/json'
+						},
+						//serialize the JSON body
+						body: JSON.stringify(Object.fromEntries(formdata))
+					})
+					const data = await response.json();
+					setMessage(data.message);
+					if (response.ok) {
+						localStorage.setItem('token', JSON.stringify(data.token));
+						history.push('/privatejournals')
+					}	
+				} catch(err) {
+					setMessage(err.message);
+				}
+			}
+			else{
+				const data = await response.json();
+				setMessage(data.message);
+			}
+			
 		} catch(err) {
 			setMessage(err.message)
 		}
